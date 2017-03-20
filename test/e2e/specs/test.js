0fc2748ffdf14c52271d524c8b9a79e2f9cc7e94
guoxiangwen <297236521@qq.com>
2017-03-20 03:11:21 +0100

    ...

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -12,7 +12,6 @@ module.exports = {
             .url("http://localhost:3001")
             .waitForElementVisible('section:first-child', 2000)
             .expect.element("header h2").text.to.equal('React Starter')
-            // .click("main > div > section:first-child > a")       
-            
+            // .click("main > div > section:first-child > a")
     }
 }
